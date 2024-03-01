b0e1b691cc51fe2290a2efcaa173a4e4e4c4f6a3
BekzodSharipbaev <python.bekzod@gmail.com>
2024-03-01 11:05:51 +0500

    'some-changes'

--- src/components/Drawer.vue
+++ src/components/Drawer.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="fixed top-0 left-0 h-full w-full bg-black z-10 opacity-70"></div>
-  <div class="bg-white w-96 h-full fixed right-0 top-0 z-20">
-    <h2>Корзина</h2>
+  <div class="bg-white w-96 h-full fixed right-0 top-0 z-20 p-8">
+    <DrawerHead />
   </div>
 </template>
+
+<script setup>
+import DrawerHead from "@/components/DrawerHead.vue";
+</script>
